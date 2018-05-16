34b104bb9de60b1827a4d27d0f014e8f8eb4ad61
Chris <tophersjones@gmail.com>
2018-05-16 15:15:06 -0500

    commit

--- game/index.js
+++ game/index.js
@@ -3,11 +3,42 @@ import {createStore} from 'redux'
 
 export default function reducer(state={turn: 'X', board: Map()}, action) {
   // TODO
-  if(action.type === 'MOVE') return {turn: state.turn === 'X' ? 'O' : 'X', board: state.board.setIn([], state.turn)}   
+  if (action.type === 'MOVE') {
+    if (action.player === 'X') {
+      ongoing['X'].push(action.position)
+    } else {
+      ongoing['O'].push(action.position)
+    }
+    winner()
+    console.log('ONGOING', ongoing)
+    return { turn: state.turn === 'X' ? 'O' : 'X', board: state.board.setIn(action.position, action.player) } 
+  } 
   return state
 }
 
-export const move = (position,player) =>{
-  return{type: 'MOVE', position, player}
+export const move = (player, position) => {
+  return {type: 'MOVE', position, player}
 }
 
+const ongoing = {
+  X: [],
+  O: []
+}
+
+function winner() {
+  let xArr = ongoing['X']
+  let oArr = ongoing['O']
+  let xCounter = 0
+  let oCounter = 0
+  for (let i = 1; i < xArr.length; i++) {
+    if (xArr[i][0] === xArr[i-1][0]) {
+      xCounter++
+    } else {
+      xCounter = 0
+    }
+    if (xCounter === 2) {
+      console.log('X wins')
+    
+    }
+  }
+}
