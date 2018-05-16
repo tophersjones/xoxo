fe7635967ad3da7e3decef2763dde62f1af3f482
Dan Gutt <dgutt10@gmail.com>
2018-05-16 13:35:36 -0500

    turn working

--- game/index.js
+++ game/index.js
@@ -1,6 +1,13 @@
 import {Map} from 'immutable'
+import {createStore} from 'redux'
 
-export default function reducer(state, action) {
+export default function reducer(state={turn: 'X', board: Map()}, action) {
   // TODO
+  if(action.type === 'MOVE') return {turn: state.turn === 'X' ? 'O' : 'X', board: state.board.setIn([], state.turn)}   
   return state
 }
+
+export const move = (position,player) =>{
+  return{type: 'MOVE', position, player}
+}
+
